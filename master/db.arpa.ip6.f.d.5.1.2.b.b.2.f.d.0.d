7cf61fee9b10309f7e2e0e16746197b071f03fbf
4ndr3 <4ndr3@users.noreply.github.com>
2014-12-05 23:18:04 +0000

    monitor.ffhh hinzugefügt

--- master/db.arpa.ip6.f.d.5.1.2.b.b.2.f.d.0.d
+++ master/db.arpa.ip6.f.d.5.1.2.b.b.2.f.d.0.d
@@ -1,7 +1,7 @@
 $ORIGIN 0.0.0.0.d.0.d.f.2.b.b.2.1.5.d.f.ip6.arpa.
 $TTL 86400	; 1 Tag
 @				IN SOA	srv01.ffhh. hostmaster.hamburg.freifunk.net. (
-				2014061500; serial: wird bei jeder Aenderung inkrementiert (Format: JJJJMMTTVV)
+				2014120501; serial: wird bei jeder Aenderung inkrementiert (Format: JJJJMMTTVV)
 				86400	; refresh: Sekundenabstand, in dem die Slaves anfragen, ob sich etwas geändert hat
 				7200	; retry: Sekundenabstand, in denen ein Slave wiederholt, falls sein Master nicht antwortet
 				3600000	; expire: wenn der Master auf einen Zonentransfer-Request nicht reagiert, deaktiviert ein Slave nach dieser Zeitspanne in Sekunden die Zone
@@ -40,3 +40,4 @@ $TTL 86400	; 1 Tag
 3.5.c.1.4.0.e.f.f.f.a.d.f.c.a.2 PTR	somakapsel.ffhh.
 e.a.7.c.c.e.e.f.f.f.b.e.7.2.a.b PTR	wp.ffhh.
 9.7.2.9.3.8.e.f.f.f.4.6.2.2.2.0 PTR	baracke.ffhh.
+3.1.0.b.1.9.e.f.f.f.0.0.4.5.0.5 PTR	monitor.ffhh.
