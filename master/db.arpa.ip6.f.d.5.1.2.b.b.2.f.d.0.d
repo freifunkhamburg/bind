bd85355ad92313f96a25cb8ce026d42b62725cb7
Alexander Dietrich <alexander@dietrich.cx>
2017-10-03 20:11:15 +0200

    Remove outdated PTR records

--- master/db.arpa.ip6.f.d.5.1.2.b.b.2.f.d.0.d
+++ master/db.arpa.ip6.f.d.5.1.2.b.b.2.f.d.0.d
@@ -1,7 +1,7 @@
 $ORIGIN 0.0.0.0.d.0.d.f.2.b.b.2.1.5.d.f.ip6.arpa.
 $TTL 86400	; 1 Tag
 @				IN SOA	srv02.ffhh. hostmaster.hamburg.freifunk.net. (
-				2017042800; serial: wird bei jeder Aenderung inkrementiert (Format: JJJJMMTTVV)
+				2017100300 ; serial: wird bei jeder Aenderung inkrementiert (Format: JJJJMMTTVV)
 				86400	; refresh: Sekundenabstand, in dem die Slaves anfragen, ob sich etwas geändert hat
 				7200	; retry: Sekundenabstand, in denen ein Slave wiederholt, falls sein Master nicht antwortet
 				3600000	; expire: wenn der Master auf einen Zonentransfer-Request nicht reagiert, deaktiviert ein Slave nach dieser Zeitspanne in Sekunden die Zone
@@ -12,10 +12,6 @@ $TTL 86400	; 1 Tag
 ;AUTHORATIVE NAMESERVERS
 @				NS      srv02.ffhh.
 ;@				NS	gw01.ffhh.
-
-
-; IPv6 von srv01
-1.0.1.0.0.0.0.0.0.0.0.0.0.0.0.0	PTR	srv01.ffhh.
 
 
 ; GATEWAYS
@@ -29,7 +25,6 @@ $TTL 86400	; 1 Tag
 1.0.5.0.0.0.0.0.0.0.0.0.0.0.0.0 PTR	gw12.ffhh.
 
 
-
 ; LOKALE DIENSTE DER FREIFUNKER_INNEN
 1.0.0.0.0.0.0.0.0.0.0.0.e.f.f.f PTR	mchai.ffhh.
 0.1.d.4.0.0.0.0.0.0.0.0.e.f.f.f PTR	jabber.ffhh.
@@ -37,4 +32,3 @@ $TTL 86400	; 1 Tag
 e.a.7.c.c.e.e.f.f.f.b.e.7.2.a.b PTR	wp.ffhh.
 3.1.0.b.1.9.e.f.f.f.0.0.4.5.0.5 PTR	monitor.ffhh.
 0.a.9.4.2.0.e.f.f.f.b.e.7.2.a.b PTR	rasfi.ffhh.
-2.6.7.2.7.5.e.f.f.f.d.9.6.6.e.7 PTR	seven.ffhh.
